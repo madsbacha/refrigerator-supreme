80de36b9a8a411104ae9bb16d1eed919273d7c8a
Mads Bach Andersen <madsbaan@gmail.com>
2019-11-13 08:02:45 +0100

    Add resolver for RateItem, CreateComment and DeleteComment

--- server/resolvers.php
+++ server/resolvers.php
@@ -88,6 +88,69 @@ return [
             }
             $data = $db->update('items', $data, compact('id'));
             return [ 'success' => $data->rowCount() > 0 ];
+        },
+        'RateItem' => function ($root, $args, $context) use ($db) {
+            if (!$context['user']) {
+                throw new Error('Unauthorized');
+            }
+            $item_id = $args['itemId'];
+            $rating = $args['rating'];
+            if (!$db->has('items', ['id' => $item_id])) {
+                throw new Error('Invalid item id');
+            }
+            if ($db->has('ratings',
+                [
+                    'item_id' => $item_id,
+                    'user_id' => $context['user']['id']
+                ]
+            )) {
+                $db->update('ratings',
+                    compact('rating'),
+                    [
+                        'user_id' => $context['user']['id'],
+                        'item_id' => $item_id
+                    ]
+                );
+            } else {
+                $db->insert('ratings', [
+                    'user_id' => $context['user']['id'],
+                    'item_id' => $item_id,
+                    'rating' => $rating
+                ]);
+            }
+            return $db->get('ratings',
+                ['id', 'item_id', 'user_id', 'rating'],
+                [
+                'user_id' => $context['user']['id'],
+                'item_id' => $item_id
+                ]
+            );
+        },
+        'CreateComment' => function ($root, $args, $context) use ($db) {
+            if (!$context['user']) {
+                throw new Error('Unauthorized');
+            }
+            $item_id = $args['itemId'];
+            $text = $args['comment'];
+            $user_id = $context['user']['id'];
+            if (!$db->has('items', ['id' => $item_id])) {
+                throw new Error('No item with that id exist');
+            }
+            $db->insert('comments', compact('item_id', 'text', 'user_id'));
+            return $db->get('comments', ['id', 'item_id', 'text', 'user_id'], ['id' => $db->lastInsertId()]);
+        },
+        'DeleteComment' => function ($root, $args, $context) use ($db) {
+            if (!$context['user']) {
+                throw new Error('Unauthorized');
+            }
+            $id = $args['commentId'];
+            $user_id = $context['user']['id'];
+            $where = compact('user_id', 'id');
+            if (!$db->has('comments', $where)) {
+                throw new Error('Cannot find or delete comment');
+            }
+            $data = $db->delete('comments', $where);
+            return [ 'success' => $data->rowCount() > 0 ];
         }
     ]
 ];
