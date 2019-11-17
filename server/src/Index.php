5745f982b0530ab6348dcf4448467112a9dcf5f7
Mads Bach Andersen <madsbaan@gmail.com>
2019-11-17 01:01:21 +0100

    Move context logic into its own method

--- server/src/Index.php
+++ server/src/Index.php
@@ -17,25 +17,6 @@ use Siler\Http\Response;
 Response\header('Access-Control-Allow-Origin', '*');
 Response\header('Access-Control-Allow-Headers', 'content-type,x-apollo-tracing');
 
-$context = new \stdClass();
-$context->User = null;
-$context->Db = new DatabaseRepository();
-$context->IsLoggedIn = false;
-
-$auth_header = Request\header('Authorization');
-if (startsWith($auth_header, 'JWT ')) {
-    $token = substr($auth_header, strlen('JWT '));
-    try {
-        $decoded = JWTHelper::decode($token);
-        if ($decoded) {
-            $context->User = $decoded->user;
-            $context->IsLoggedIn = true;
-        }
-    } catch (\Exception $exception) {
-        // pass
-    }
-}
-
 // Respond only for POST requests
 if (Request\method_is('post')) {
     // Retrieve the Schema
@@ -46,9 +27,31 @@ if (Request\method_is('post')) {
     ]);
 
     // Give it to siler
-    GraphQL\init($schema, null, $context);
+    GraphQL\init($schema, null, getContext());
 }
 
 function startsWith($str, $needle) {
     return substr($str, 0, strlen($needle)) === $needle;
+}
+
+function getContext() {
+    $context = new \stdClass();
+    $context->User = null;
+    $context->Db = new DatabaseRepository();
+    $context->IsLoggedIn = false;
+
+    $auth_header = Request\header('Authorization');
+    if (startsWith($auth_header, 'JWT ')) {
+        $token = substr($auth_header, strlen('JWT '));
+        try {
+            $decoded = JWTHelper::decode($token);
+            if ($decoded) {
+                $context->User = $decoded->user;
+                $context->IsLoggedIn = true;
+            }
+        } catch (\Exception $exception) {
+            // pass
+        }
+    }
+    return $context;
 }
