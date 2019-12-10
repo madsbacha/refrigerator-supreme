f1a92a6ac8388a01f31d22dccbfa7e63c6667c65
Matthias Hansen <matnhansen@gmail.com>
2019-12-10 01:08:21 +0100

    Refactored RealHome to use components

---
+++ client/src/components/HomePage/LeftColumn.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container__left">
+        <div class="flex justify-center items-center">
+            <div class="flex flex-col">
+                <h1 class="heading">Life is too short for unpleasent beverages.</h1>
+                <p class="mt-16">
+                    Multiple students have carelessly chosen between a collection of beverages to only be disapointed
+                    with their experience.
+                </p>
+                <p>
+                    At <strong>Refrigerator Supreme</strong> we seek to find the best beverage available at Cassiopeia
+                    to never let anyone down again.
+                </p>
+                <Button class="w-1/4 mt-8" text="Start voting"/>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import Button from '../Button'
+
+export default {
+  name: 'LeftColumn',
+  components: { Button }
+}
+</script>
+<style lang="scss" scoped>
+  .container {
+    &__left {
+      background-color: #080808;
+      color: #fff;
+      @apply p-16 flex w-1/2 h-screen;
+
+      p {
+        @apply text-xl pb-12;
+      }
+    }
+  }
+
+  .heading {
+    @apply text-6xl font-light;
+  }
+</style>
---
+++ client/src/components/HomePage/RightColumnItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <div :class="['container__right-' + level]">
+    <div class="flex flex-col justify-center items-center h-full">
+      <div class="uppercase tracking-widest">{{ leadingText }}</div>
+      <h2 class="subheading">{{ heading }}</h2>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Upper',
+  props: {
+    level: {
+      type: String,
+      required: true
+    },
+    leadingText: String,
+    heading: String
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .container {
+    &__right {
+      @apply w-1/2 flex flex-col text-center text-white;
+
+      &-upper,
+      &-lower {
+        @apply flex-auto;
+      }
+
+      &-upper {
+        @apply orange;
+      }
+
+      &-lower {
+        @apply yellow;
+      }
+    }
+  }
+
+  .subheading {
+    @apply text-5xl font-light;
+  }
+
+  .uppercased {
+    @apply uppercase;
+  }
+
+  .orange {
+    background-color: #F17849;
+  }
+
+  .yellow {
+    background-color: #FAB131;
+  }
+</style>
