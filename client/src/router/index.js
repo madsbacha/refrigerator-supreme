959382466286c1cadee9a35c22912038d3cc6a7c
Matthias Hansen <matnhansen@gmail.com>
2019-12-10 01:07:47 +0100

    Added RealHome component to router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import RealHome from '../views/RealHome'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/realhome',
+    name: 'realhome',
+    component: RealHome
   },
   {
     path: '/login',
